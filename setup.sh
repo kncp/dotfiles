3dc4b023e79a65c2201e394aa85247a2d2d1a189
kncp <k.goto.bn@gmail.com>
2019-11-08 19:20:54 +0900

    fix initialize script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+# setup prezto
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# update rc
+
+cat \<<EOF >> ~/.zshrc
+# path
+[ -f ~/dotfiles/.zshrc.path ] && source ~/dotfiles/.zshrc.path
+
+# opt
+[ -f ~/dotfiles/.zshrc.opt ] && source ~/dotfiles/.zshrc.opt
+
+# alias
+[ -f ~/dotfiles/.zshrc.alias ] && source ~/dotfiles/.zshrc.alias
+
+# history
+[ -f ~/dotfiles/.zshrc.history ] && source ~/dotfiles/.zshrc.history
+
+# base
+[ -f ~/dotfiles/.zshrc.base ] && source ~/dotfiles/.zshrc.base
+
+# local
+[ -f ~/.zshrc.local ] && source ~/.zshrc.local
+EOF
+
 # initialize dotfiles
 
-DOT_FILES=( .zshrc .gitconfig .gitignore_global )
+DOT_FILES=( .gitconfig .gitignore_global )
 
 for file in ${DOT_FILES[@]}
 do
